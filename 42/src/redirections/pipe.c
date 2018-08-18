c8c5775dd14504a55b6cd432c7461c65d9ae702e
Jeremy Combe <jeremcombe@hotmail.fr>
2018-08-18 13:20:36 +0200

    FINI LE PIPE

--- 42/src/redirections/pipe.c
+++ 42/src/redirections/pipe.c
@@ -58,30 +58,17 @@ tmp = tmp->next;
 return (ret);
 }*/
 
-int g_i;
 
 void		fils_pute(t_op *op, int *fd, int fd_out)
 {	char *bin;
 	//ft_get_bin();
 	bin = ft_search_bin(op->cmd[0]);
-	if (g_i == 1)
+	if (op->next)
 	{
 		dup2(fd[1], STDOUT_FILENO);
 	}
 	close(fd[0]);
 	execve(bin, op->cmd, g_env);
-	//exit(-1);
-}
-void		closee(int *p, int fd_in, int fd_out, t_op *op, int fd_save)
-{
-	if (fd_in > 0)
-		close(fd_in);
-	if (fd_out != 1)
-		close(fd_out);
-	close(p[1]);
-	if (fd_save)
-		close(fd_save);
-	fd_save = p[0];
 }
 
 int			waitstat(int *status)
@@ -99,10 +86,8 @@ int		ft_pipe_execute(int i, t_op *op, pid_t pidd, int *fd_pipe)
 	int fd_out = 1;
 	int fd_in = 0;
 
-	g_i = 0;
 	while (op)
 	{
-		g_i++;
 		pipe(fd);
 		fd_out =  1;
 		fd_in = 0;
@@ -116,14 +101,13 @@ int		ft_pipe_execute(int i, t_op *op, pid_t pidd, int *fd_pipe)
 		else
 		{	
 			if (fd_in > 0)
-			close(fd_in);
+				close(fd_in);
 			if (fd_out != 1)
 				close(fd_out);
 			close(fd[1]);
 			if (fd_save)
 				close(fd_save);
 			fd_save = fd[0];
-			//	closee(fd, fd_in, fd_out, op, fd_save);
 			op = op->next;
 		}
 
@@ -131,38 +115,6 @@ int		ft_pipe_execute(int i, t_op *op, pid_t pidd, int *fd_pipe)
 	status = waitstat(&status);
 	WEXITSTATUS(status);
 	return (0);
-
-
-	/*t_op *tmp;
-	  int fd[2];
-	  int fdd[7];
-	  pid_t pid;
-	  int ret = 0;
-	//pid_t pid;
-	if (op->next == NULL)
-	{	char *bin;
-	ft_get_bin();
-	bin = ft_search_bin(op->cmd[0]);
-	printf("OKNULL %s\n", op->cmd[0]);
-	return (execve(bin, op->cmd, g_env));
-	}
-	if (!op)
-	return (0);
-	pipe(fd);
-	pid = fork();
-	if (pid == 0)
-	fils_pute(op, fd);
-	else
-	{
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[1]);
-	return (ft_pipe_execute(i, op->next, pidd,fdd));
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid, NULL, 0);
-	return (0);
-	*/
 }
 
 
