9f91345e20b98744a15faae56d2d97da92fe1827
Jeremy Combe <jeremcombe@hotmail.fr>
2018-08-30 01:03:12 +0200

    mise a la norme

--- src/redirections/loop_redirections.c
+++ src/redirections/loop_redirections.c
@@ -18,6 +18,7 @@ typedef struct s_loop
 	int fd;
 	int i;
 	int flag;
+	int flag2;
 }				t_loop;
 
 int ft_loop_on_heredoc(t_redirect *redirect, int flag, char *bin_cmd, pid_t cpid)
@@ -29,42 +30,50 @@ int ft_loop_on_heredoc(t_redirect *redirect, int flag, char *bin_cmd, pid_t cpid
 				return (EXIT_FAILURE);
 }
 
-int g_fd_open;;
+int g_fd_open;
+
+int				ft_loop_3(t_loop **loop, pid_t cpid, t_redirect **redirect, char *bin_cmd)
+{
+	if ((*loop)->flag == O_RDONLY)
+			if (ft_check_source((**redirect).file) == -1)
+				return (EXIT_FAILURE);
+		if ((**loop).flag == HEREDOC && ft_loop_on_heredoc((*redirect), (**loop).flag, bin_cmd, cpid) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
+		(**loop).fd = (*redirect)->fd;
+		if ((**loop).flag != NOTHING && (**loop).flag != HEREDOC)
+		{
+			if ((**loop).flag != O_RDONLY)
+				if ((ft_check_file_is_directory((**redirect).file) == -1))
+					return (EXIT_FAILURE);
+		}
+		if ((**loop).fd != NOTHING)
+		{
+			if ((**loop).flag == O_RDONLY)
+				(**loop).flag2 = O_RDONLY;
+			else
+				(**loop).flag2 = O_WRONLY;
+		}
+	return (EXIT_SUCCESS);
+	}
 int				ft_loop_2(t_redirect *redirect, t_loop **loop, char *bin_cmd, int flag2, int buil)
 {
 	pid_t cpid;
 	int fd_open;
 	int i = 0;
+	int ret;
 	while (redirect)
 	{
 		(*loop)->i++;
 		(*loop)->flag = ft_return_flag(redirect);
-		if ((*loop)->flag == O_RDONLY)
-			if (ft_check_source(redirect->file) == -1)
-				return (EXIT_FAILURE);
-		if ((*loop)->flag == HEREDOC && ft_loop_on_heredoc(redirect, (*loop)->flag, bin_cmd, cpid) == EXIT_FAILURE)
-			break;
-		(*loop)->fd = redirect->fd;
-		if ((*loop)->flag != NOTHING && (*loop)->flag != HEREDOC)
-		{
-			if ((*loop)->flag != O_RDONLY)
-				if (ft_check_file_is_directory(redirect->file) == -1)
-					return (EXIT_FAILURE);
-		}
-		if ((*loop)->fd != NOTHING)
-		{
-			if ((*loop)->flag == O_RDONLY)
-				flag2 = O_RDONLY;
-			else
-				flag2 = O_WRONLY;
-		}
+		if ((ret = ft_loop_3(&(*loop), cpid, &redirect, bin_cmd)) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
 		if (redirect->redirect != DLESS)
-			fd_open = ft_open_redirect(redirect->file, (*loop)->flag, flag2, redirect->fd);
+			fd_open = ft_open_redirect(redirect->file, (*loop)->flag, (*loop)->flag2, redirect->fd);
 		redirect = redirect->next;
 	}
 	if (buil == 1)
 	return (fd_open);
-	if (flag2 == O_RDONLY)
+	if ((*loop)->flag2 == O_RDONLY)
 		dup2(fd_open, 0);
 	else
 		dup2(fd_open, (*loop)->fd);
@@ -86,16 +95,7 @@ int				ft_loop(t_redirect *redirect, t_loop *loop, int flag2, int buil, char *bi
 			return (EXIT_FAILURE);
 	if (buil == 1)
 		return (fd_open);
-	/*if (redirect == NULL && loop->flag == HEREDOC && loop->i == 1)
-	{
-		printf("fini heredoc\n");
-		return (EXIT_SUCCESS);
-	}*/
-	/*if (flag2 == O_RDONLY)
-		dup2(fd_open, 0);
-	else
-		dup2(g_fd_open, g_fd);
-*/	return(EXIT_SUCCESS);
+		return(EXIT_SUCCESS);
 }
 
 
@@ -111,6 +111,7 @@ int				ft_loop_redirect(t_redirect *redirect, char *bin_cmd, pid_t cpid, int bui
 	loop.fd = 0;
 	loop.i = 0;
 	loop.flag = 0;
+	loop.flag2 = 0;
 
 	i = 0;
 	if (fd2 > 1)
