a800c2ac1edb431716aa62133e64cebe4f465117
dz0nda <dzonda@student.42.fr>
2018-09-07 01:04:23 +0200

    libft update

--- libft/src/ft_tabdup.c
+++ libft/src/ft_tabdup.c
@@ -6,7 +6,7 @@
 /*   By: gmadec <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/30 05:52:29 by gmadec       #+#   ##    ##    #+#       */
-/*   Updated: 2018/08/12 04:10:08 by gmadec      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/07 01:00:10 by dzonda      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,18 +18,14 @@ char		**ft_tabdup(char **tab)
 	char		**ret;
 	int			i;
 
-	i = 0;
+	i = -1;
 	ret = NULL;
-	if (tab)
-	{
-		if (!(ret = malloc(sizeof(char**) * ft_tablen(tab) + 1)))
-			return (NULL);
-		while (tab[i])
-		{
-			ret[i] = ft_strdup(tab[i]);
-			i++;
-		}
-		ret[i] = NULL;
-	}
+	if (!tab)
+		return (NULL);
+	if (!(ret = malloc(sizeof(char**) * ft_tablen(tab) + 1)))
+		return (NULL);
+	while (tab[++i])
+		ret[i] = ft_strdup(tab[i]);
+	ret[i] = NULL;
 	return (ret);
 }
