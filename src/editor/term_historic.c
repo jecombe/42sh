d9ac6bae8da296af5367718548ce794ed0a5bf7b
guyb27 <madec.guillaume@hotmail.fr>
2018-09-18 04:24:52 +0200

    HISTORIC FINISH

--- src/editor/term_historic.c
+++ src/editor/term_historic.c
@@ -6,7 +6,7 @@
 /*   By: dewalter <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/08/01 10:01:52 by dewalter     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/17 11:25:01 by gmadec      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/18 04:23:43 by gmadec      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -28,28 +28,24 @@ static void	print_str(t_editor **ed)
 
 static int	down_key(t_editor **ed)
 {
-	if ((*ed)->hist >= 0)
+	(*ed)->hist--;
+	if ((*ed)->hist < 0)
 	{
-		(*ed)->hist--;
-		if ((*ed)->hist == -1)
+		ft_strdel(&(*ed)->line);
+		go_to_begin_of_line(*ed);
+		delete_from_cursor_to_end(*ed);
+		if ((*ed)->tmp_line)
 		{
 			ft_strdel(&(*ed)->line);
-			if ((*ed)->tmp_line)
-			{
-				go_to_begin_of_line(*ed);
-				delete_from_cursor_to_end(*ed);
-				ft_strdel(&(*ed)->line);
-				(*ed)->line = ft_strdup((*ed)->tmp_line);
-				ft_strdel(&(*ed)->tmp_line);
-				print_str(ed);
-				(*ed)->cursor_str_pos = ft_strlen((*ed)->line);
-			}
-			(*ed)->hist = -2;
-			return (0);
+			(*ed)->line = ft_strdup((*ed)->tmp_line);
+			ft_strdel(&(*ed)->tmp_line);
+			print_str(ed);
 		}
-	}
-	else
+		(*ed)->cursor_str_pos = (*ed)->line ? ft_strlen((*ed)->line) : 0;
+		(*ed)->hist = -2;
 		return (0);
+	}
+	(*ed)->hist--;
 	return (1);
 }
 
