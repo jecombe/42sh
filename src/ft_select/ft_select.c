b86356d773c26241dfeb5b9540151589d49e64ef
guyb27 <madec.guillaume@hotmail.fr>
2018-09-23 03:02:31 +0200

    TAB EN COURS, PRESQUE EN ETAT DE FONCTIONNEMENT FT_SELECT OK REMETTRE LA LIGNE AU DESSUS LES LEAKS && FINISH THE PROJECT

--- src/ft_select/ft_select.c
+++ src/ft_select/ft_select.c
@@ -6,7 +6,7 @@
 /*   By: gmadec <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/01 04:22:27 by gmadec       #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/22 22:08:18 by gmadec      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/23 02:45:22 by gmadec      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,24 +15,24 @@
 
 static int	ft_manage_raw(t_term *raw, int version)
 {
-/*	if (version == 0)
-	{
+	/*	if (version == 0)
+		{
 		if (tcgetattr(STDIN_FILENO, raw) == -1)
-			return (1);
+		return (1);
 		(*raw).c_cc[VMIN] = 1;
 		(*raw).c_cc[VTIME] = 0;
 		if (tcsetattr(STDIN_FILENO, TCSANOW, raw) == -1)
-			return (1);
+		return (1);
 		tputs(tgetstr("vi", NULL), 1, ft_outc);
-	}
-	else
-	{
+		}
+		else
+		{
 		(*raw).c_cc[VMIN] = 0;
 		(*raw).c_cc[VTIME] = 1;
 		if (tcsetattr(STDIN_FILENO, TCSANOW, raw) == -1)
-			return (1);
+		return (1);
 		tputs(tgetstr("ve", NULL), 1, ft_outc);
-	}*/
+		}*/
 	return (0);
 }
 
@@ -48,19 +48,19 @@ int				ft_test(t_select **sel, int ret, t_editor **ed)
 	int			refresh;
 
 	refresh = 0;
-//	tputs(tgetstr("cd", NULL), 1, ft_outc);
-//	while ((*sel && ret != 4) || ret == 0)
-//	{
+	//	tputs(tgetstr("cd", NULL), 1, ft_outc);
+	//	while ((*sel && ret != 4) || ret == 0)
+	//	{
 	//	refresh = ft_refresh((*sel), ret);
-//		if (refresh == 1)
-//		{
-//		printf("REFRESH == %d\n", refresh);
-//		sleep(2);
-//			ft_print_params(*sel);
-//		}
-//		refresh == 2 ? ft_putstr_fd("\E[0J", 2) : 0;
-		ret = ft_manage_entry(ed, &(*sel));
-//	}
+	//		if (refresh == 1)
+	//		{
+	//		printf("REFRESH == %d\n", refresh);
+	//		sleep(2);
+	//			ft_print_params(*sel);
+	//		}
+	//		refresh == 2 ? ft_putstr_fd("\E[0J", 2) : 0;
+	ret = ft_manage_entry(ed, &(*sel));
+	//	}
 	return (0);
 }
 
@@ -70,15 +70,12 @@ int			ft_select(t_editor **ed, char **line, int version)
 	t_term		raw;
 
 	ret = 0;
-	if (!ft_manage_raw(&raw, 0))
-	{
-		version == 0 ? ft_init_select(&(*ed)->sel, (*ed)->t.elem, (*ed)->tabu) : 0;
-		version == 0 ? ft_print_params((*ed)->sel) : 0;
-		version == 1 ? ft_test(&(*ed)->sel, ret, ed) : 0;
-		if ((*ed)->sel->ret)
-			*line = ft_strdup((*ed)->sel->ret);
-		ft_manage_raw(&raw, 1);
-//		ft_free_t_select(&sel);
-	}
+	version == 0 ? ft_init_select(&(*ed)->sel, (*ed)->t.elem, (*ed)->tabu) : 0;
+	version == 0 ? ft_print_params((*ed)->sel) : 0;
+	version == 1 ? ft_manage_entry(ed, &(*ed)->sel) : 0;
+//	version == 1 ? ft_test(&(*ed)->sel, ret, ed) : 0;
+	if ((*ed)->sel->ret)
+		*line = ft_strdup((*ed)->sel->ret);
+	ft_manage_raw(&raw, 1);
 	return (0);
 }
