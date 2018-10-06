e9d11a2be3b1ffd7c3a8253c829c7a7c32df2a40
Denis Walter <deness67@live.fr>
2018-10-06 20:58:27 +0200

    putfreshstr update

--- src/editor/ft_putfreshstr.c
+++ src/editor/ft_putfreshstr.c
@@ -6,7 +6,7 @@
 /*   By: gmadec <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/25 03:50:17 by gmadec       #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/05 16:29:25 by dewalter    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/06 20:53:47 by dewalter    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,13 +15,9 @@
 
 void		ft_putfreshstr(char *str)
 {
-	int		i;
-
-	i = -1;
 	if (str)
-		while (str[++i])
-		{
-			tputs(tgetstr("ce", NULL), 1, ft_putchar);
-			write(1, &str[i], 1);
-		}
+	{
+		tputs(tgetstr("ce", NULL), 1, ft_putchar);
+		write(1, str, ft_strlen(str));
+	}
 }
